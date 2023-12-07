4388b864809a8cf5e82d31cf7457b0cff39e3884
Koleduke <ydel2014@gmail.com>
2023-12-07 20:12:26 +0300

    done1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,8 +1,7 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 
 
@@ -13,25 +12,7 @@ public class FileReader {
     private Long phoneR;
 
     public Profile getDataFromFile(File file)  {
-        java.io.FileReader sdf = null;
-        String input ="";
-        try {
-            int ch;
-            sdf = new java.io.FileReader(file);
-            while ((ch=sdf.read())!=-1){
-                String str = Character.toString( (char) ch);
-                //System.out.println(ch);
-            input = input+str;}
-        } catch (IOException e){}
-        finally {
-            assert sdf != null;
-            try {
-                sdf.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        String[] sentences = input.split("\\\n");
+        String[] sentences = getStrings(file);
         for (int i = 0; i<sentences.length;i++ ){
             String[] sentences1 = sentences[i].split("\\s+");
             if(i==0){
@@ -44,6 +25,29 @@ public class FileReader {
             phoneR = Long.valueOf(sentences1[1]);}
         }
                 return new Profile(nameR,ageR,emailR,phoneR);
+    }
+
+    private static String[] getStrings(File file) {
+        java.io.FileReader sdf = null;
+        String input ="";
+        try {
+            int ch;
+            sdf = new java.io.FileReader(file);
+            while ((ch=sdf.read())!=-1){
+                String str = Character.toString( (char) ch);
+                //System.out.println(ch);
+                input = input + str;}
+        } catch (IOException e){}
+        finally {
+            assert sdf != null;
+            try {
+                sdf.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        String[] sentences = input.split("\\\n");
+        return sentences;
     }
 }
 
